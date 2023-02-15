d600841f42f81800cc4cb77bd6c60b46704ef5bf
Issao Takeuchi <issaottakeuchi@gmail.com>
2023-02-15 20:55:00 -0300

    Add CRUD methods on CategoriaService class.

--- back-end/zumba-squad/src/main/java/com/example/zumbasquad/service/CategoriaService.java
+++ back-end/zumba-squad/src/main/java/com/example/zumbasquad/service/CategoriaService.java
@@ -10,4 +10,21 @@ public class CategoriaService {
     public CategoriaService(ICategoriaRepository repository) {
         this.repository = repository;
     }
+
+    public Categoria add(Categoria categoria) {
+        return repository.save(categoria);
+    }
+
+    public List<Categoria> getAll() {
+        return repository.findAll();
+    }
+
+
+    public void remove(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Categoria update(Categoria categoria) {
+        return repository.saveAndFlush(categoria);
+    }
 }
