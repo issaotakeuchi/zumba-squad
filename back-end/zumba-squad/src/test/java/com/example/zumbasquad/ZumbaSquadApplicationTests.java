1b56068e21ef9327792e65a426798e28168a6061
Caroline Machado da Silva <ms.caroline@hotmail.com>
2023-02-14 21:33:57 -0300

    feat: first commit

---
+++ back-end/zumba-squad/src/test/java/com/example/zumbasquad/ZumbaSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zumbasquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZumbaSquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
