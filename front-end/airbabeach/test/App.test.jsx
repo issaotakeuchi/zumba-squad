ca62ff0fc7623548e80381ed9d6c7cd4089e4797
AndrePedreschi <a.pedreschi32@gmail.com>
2023-02-24 19:44:18 -0300

    Testes unitários dos componentes e mensageria

--- front-end/airbabeach/test/App.test.jsx
+++ front-end/airbabeach/test/App.test.jsx
@@ -1,25 +1,86 @@
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-/*
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { render, screen, fireEvent, waitFor, getByRole } from './test-utils.jsx';
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
-}); */
-
-
-//import App from './App';
+import { Home } from '../src/pages/Home';
 import { Footer } from '../src/Components/Footer'
+import { Header } from '../src/Components/Header'
+import { Form } from '../src/Components/Form'
 
-describe('Footer', () => {
+describe('Home component', () => {
   it('É renderizado corretamente"', () => {
-    render(<Footer />);
+    render(<Home />);
     //screen.debug();
-    expect(screen.getByText(/©2023 AirBnBeach, inc/)).toBeInTheDocument();
-    expect(screen.getByText(/©2023 AirBnBeach, inc/)).toMatchSnapshot()
+    expect(screen.getByText('Home')).toBeInTheDocument();
   });
 });
+
+
+describe('Header component', () => {
+  it('É renderizado corretamente"', () => {
+    render(<Header />);
+    //screen.debug();
+    expect(screen.getByText('header')).toBeInTheDocument();
+    //expect(screen.getByText(/©2023 AirBnBeach, inc/)).toMatchSnapshot()
+    //expect(screen.getByText(/Content/i)).toBeUndefined()
+  });
+});
+
+
+describe('Footer component', () => {
+  it('É renderizado corretamente"', () => {
+    render(<Footer />);
+    expect(screen.getByText(/©2023 AirBnBeach, inc/)).toBeInTheDocument();
+
+  });
+});
+
+describe('Login-Form component', () => {
+  it('É renderizado corretamente"', () => {
+    render(<Form type='login' />);
+    //screen.debug();
+    expect(screen.getByText(/Iniciar Sessão/)).toBeInTheDocument();
+
+  });
+});
+
+describe('CreateUser-Form component', () => {
+  it('É renderizado corretamente', () => {
+    render(<Form type='createUser' />);
+    //Tratativa para lidar com múltiplos elementos quando se quer um em específico 
+    expect(screen.getAllByText(/Criar conta/)[0]).toBeInTheDocument();
+  }
+  );
+
+
+
+  /*  it("Clicar no botão login é direcionado a página de login", async () => {
+     
+     render(<Form type='createUser'/>);
+     
+ 
+     fireEvent.click(screen.getByTestId('Homac'))
+     //fireEvent.click(screen.getByText('Soma'))
+ 
+     
+     expect(screen.debug())
+ 
+ 
+     
+       
+     //screen.debug()
+     //expect(screen.getByText('Home')).toBeInTheDocument();
+ 
+     
+ }); */
+
+
+
+});
+
+
+
+
+
+
+
+
