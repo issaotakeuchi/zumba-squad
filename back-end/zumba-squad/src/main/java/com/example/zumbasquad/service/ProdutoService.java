9d3469654a6a51db000c325c96033a0d46933826
Issao Takeuchi <issaottakeuchi@gmail.com>
2023-03-02 21:29:36 -0300

    add: class ProdutoService.

---
+++ back-end/zumba-squad/src/main/java/com/example/zumbasquad/service/ProdutoService.java
@@ -0,0 +1,21 @@
+package com.example.zumbasquad.service;
+
+import com.example.zumbasquad.model.Produto;
+import com.example.zumbasquad.repository.IProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    private final IProdutoRepository repository;
+
+    public ProdutoService(IProdutoRepository repository) { this.repository = repository; }
+
+    public Produto add(Produto produto) { return repository.save(produto); }
+
+    public List<Produto> getAll() { return repository.findAll(); }
+
+    public Produto getById(Long id) { return repository.findById(id).get(); }
+
+}
