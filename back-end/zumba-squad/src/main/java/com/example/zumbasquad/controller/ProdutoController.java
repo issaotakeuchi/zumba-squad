b51bd27336f285062417217f4599d727b0e42c35
Issao Takeuchi <issaottakeuchi@gmail.com>
2023-03-02 21:41:24 -0300

    add: class ProdutoController.

---
+++ back-end/zumba-squad/src/main/java/com/example/zumbasquad/controller/ProdutoController.java
@@ -0,0 +1,55 @@
+package com.example.zumbasquad.controller;
+
+import com.example.zumbasquad.exceptions.BadRequestException;
+import com.example.zumbasquad.exceptions.ResourceNotFoundException;
+import com.example.zumbasquad.model.Produto;
+import com.example.zumbasquad.service.ProdutoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+    private final ProdutoService service;
+    final static Logger log = LoggerFactory.getLogger(ProdutoController.class);
+
+    public ProdutoController(ProdutoService service) { this.service = service; }
+
+    @PostMapping
+    public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto) throws BadRequestException {
+        try {
+            log.info("Cadastrado novo produto com sucesso.");
+            return  ResponseEntity.ok(service.add(produto));
+        } catch (Exception e) {
+            log.info("Não foi possível cadastrar o produto com base nas informações recebidas.");
+            throw new BadRequestException("Não foi possível cadastrar o produto com base nas informações recebidas.");
+        }
+    }
+
+    @GetMapping
+    public List<Produto> buscarTodos() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarProdutoId(@PathVariable Long id) throws ResourceNotFoundException {
+        try {
+            service.getById(id);
+            log.info("Encontrado o produto solicitado de id: " + id);
+            return ResponseEntity.ok(service.getById(id));
+        } catch (Exception e) {
+            log.info("Não foi encontrado o produto solicitado de id: " + id);
+            throw new ResourceNotFoundException("Não foi encontrado o produto solicitado de id: " + id);
+        }
+    }
+
+    @ExceptionHandler({BadRequestException.class})
+    public ResponseEntity<String> processErrorBadRequest(BadRequestException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
